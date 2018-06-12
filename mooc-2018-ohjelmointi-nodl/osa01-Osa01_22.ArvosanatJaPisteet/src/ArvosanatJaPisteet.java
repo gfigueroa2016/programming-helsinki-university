fd70a6b38e52c3871283862b7c735c98c296215c
Gabriel Figueroa <gabriel.figueroa2020@gmail.com>
2018-06-12 15:19:38 -0400

    Completed Exercise 22

--- mooc-2018-ohjelmointi-nodl/osa01-Osa01_22.ArvosanatJaPisteet/src/ArvosanatJaPisteet.java
+++ mooc-2018-ohjelmointi-nodl/osa01-Osa01_22.ArvosanatJaPisteet/src/ArvosanatJaPisteet.java
@@ -4,7 +4,47 @@ import java.util.Scanner;
 public class ArvosanatJaPisteet {
 
     public static void main(String[] args) {
+
         Scanner lukija = new Scanner(System.in);
 
+        System.out.println("Enter score [0-100]: ");
+
+        int arvosana = Integer.parseInt(lukija.nextLine());
+
+        if (arvosana >= 0 && arvosana <= 69) {
+
+            System.out.println("\n\nArvosana: hylätty");
+
+        } else if (arvosana >= 70 && arvosana <= 75) {
+
+            System.out.println("\n\nArvosana: 1");
+
+        } else if (arvosana >= 76 && arvosana <= 80) {
+
+            System.out.println("\n\nArvosana: 2");
+
+        } else if (arvosana >= 81 && arvosana <= 85) {
+
+            System.out.println("\n\nArvosana: 3");
+
+        } else if (arvosana >= 86 && arvosana <= 90) {
+
+            System.out.println("\n\nArvosana: 4");
+
+        } else if (arvosana >= 91 && arvosana <= 100) {
+
+            System.out.println("\n\nArvosana: 5");
+
+        } else if (arvosana > 100) {
+
+            System.out.println("\n\nArvosana: uskomatonta!");
+
+        } else {
+
+            System.out.println("\n\nArvosana: mahdotonta!");
+
+        }
+
     }
+    
 }
