c30b96806ed16c0f3c8a1e986dc0fb3faf74e3b4
Gabriel Figueroa <gabriel.figueroa2020@gmail.com>
2018-06-12 13:46:31 -0400

    Completed Exercise 21

--- mooc-2018-ohjelmointi-nodl/osa01-Osa01_21.SuurempiTaiYhtasuuri/src/SuurempiTaiYhtasuuri.java
+++ mooc-2018-ohjelmointi-nodl/osa01-Osa01_21.SuurempiTaiYhtasuuri/src/SuurempiTaiYhtasuuri.java
@@ -4,7 +4,32 @@ import java.util.Scanner;
 public class SuurempiTaiYhtasuuri {
 
     public static void main(String[] args) {
+
         Scanner lukija = new Scanner(System.in);
 
+        System.out.println("Anna ensimmäinen luku: ");
+        int luku1 = Integer.parseInt(lukija.nextLine());
+        
+        System.out.println("Anna toinen luku: ");
+        int luku2 = Integer.parseInt(lukija.nextLine());
+
+        if (luku1 > luku2) {
+
+            System.out.println("Anna ensimmäinen luku: " + luku1 + "\nAnna toinen luku: " + luku2 + "\n\n"
+                + "Suurempi luku: " + luku1);
+
+        } else if (luku1 < luku2) {
+
+            System.out.println("Anna ensimmäinen luku: " + luku1 + "\nAnna toinen luku: " + luku2 + "\n\n"
+                + "Suurempi luku: " + luku2);
+
+        } else {
+
+            System.out.println("Anna ensimmäinen luku: " + luku1 + "\nAnna toinen luku: " + luku2 + "\n\n"
+                    + "Luvut ovat yhtä suuret!");
+
+        }
+
     }
+    
 }
