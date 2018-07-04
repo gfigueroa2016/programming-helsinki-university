b21345e8e5610991a608a5562b066c704652bcb4
Gabriel Figueroa <gabriel.figueroa2020@gmail.com>
2018-07-04 12:58:02 -0400

    New exercises from part 3

---
+++ mooc-2018-ohjelmointi-nodl/osa03-Osa03_08.NimenPituus/src/NimenPituus.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class NimenPituus {
+    public static void main(String[] args) {
+        Scanner lukija = new Scanner(System.in);
+        // kutsu täältä metodiasi
+    }
+    
+    // tee tänne metodi 
+    // public static int laskeKirjaimet(String merkkijono)
+    
+}
